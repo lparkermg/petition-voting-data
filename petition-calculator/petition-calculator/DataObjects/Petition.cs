58bd6d4ad6f5e2ec0ec2e3c8a0a5cec1fc63bcb4
Luke Parker <moongateuk@gmail.com>
2016-06-25 21:47:17 +0100

    Done logic.

--- petition-calculator/petition-calculator/DataObjects/Petition.cs
+++ petition-calculator/petition-calculator/DataObjects/Petition.cs
@@ -10,13 +10,17 @@ namespace petition_calculator.DataObjects
     {
         public string Action { get; private set; }
         public string Url { get; private set; }
+        public DateTime LastUpdated { get; private set; }
         public List<Country> Countries { get; private set; }
         public List<Constituency> Constituencies { get; private set; }
+        public int TotalVotes { get; private set; }
 
-        public Petition(string action, string url, List<Country> countries, List<Constituency> constituencies)
+        public Petition(string action, string url,DateTime lastUpdated,int totalVotes, List<Country> countries, List<Constituency> constituencies)
         {
             Action = action;
             Url = url;
+            LastUpdated = lastUpdated;
+            TotalVotes = totalVotes;
             Countries = countries;
             Constituencies = constituencies;
         }
--- petition-calculator/petition-calculator/Program.cs
+++ petition-calculator/petition-calculator/Program.cs
@@ -1,71 +1,142 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using petition_calculator.DataObjects;
 
 namespace petition_calculator
 {
     class Program
     {
-        private string _petitionJsonUrl = "";
+        private static string _petitionJsonUrl = "https://petition.parliament.uk/petitions/131215.json";
+        private static Petition _petition;
 
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Petition data calculator. By Luke Parker");
             TagsUsing(args);
+            Console.WriteLine("\n Downloading Json Data.");
+            
+            _petition = GetJsonData();
+
+            Console.WriteLine("Crunching Data.");
+            RunCalculations(args);
+            Console.WriteLine("Done!");
+            Console.ReadLine();
+        }
+
+        private static Petition GetJsonData()
+        {
+            var jsonData = "";
+
+            using (WebClient wb = new WebClient())
+            {
+                jsonData = wb.DownloadString(_petitionJsonUrl);
+            }
+
+            if (!String.IsNullOrWhiteSpace(jsonData))
+            {
+                var dyn = JsonConvert.DeserializeObject<dynamic>(jsonData);
+
+                var countries = GetCountries(dyn.data.attributes.signatures_by_country);
+                var constituencies = GetContituencies(dyn.data.attributes.signatures_by_constituency);
+
+                return new Petition(dyn.data.attributes.action.ToString(),_petitionJsonUrl,DateTime.Parse(dyn.data.attributes.updated_at.ToString()),Convert.ToInt32(dyn.data.attributes.signature_count.ToString()),countries,constituencies);
+            }
+
+            return null;
+        }
+
+        private static List<Country> GetCountries(dynamic countries)
+        {
+            var theCountries = new List<Country>();
+
+            foreach (var country in countries)
+            {
+                theCountries.Add(new Country(country.name.ToString(), country.code.ToString(), Convert.ToInt32(country.signature_count.ToString())));
+            }
+
+            return theCountries;
+        }
+
+        private static List<Constituency> GetContituencies(dynamic constituencies)
+        {
+            var theConstituencies = new List<Constituency>();
+
+            foreach (var constituency in constituencies)
+            {
+                theConstituencies.Add(new Constituency(constituency.name.ToString(),constituency.ons_code.ToString(),constituency.mp.ToString(),Convert.ToInt32(constituency.signature_count.ToString())));
+            }
+
+            return theConstituencies;
         }
 
         private static void TagsUsing(string[] args)
         {
             foreach (var arg in args)
             {
-                switch (arg)
+                if (arg.Contains("-country-top-"))
                 {
-                    case ("-country-top-5"):
-                        Console.WriteLine("Top Five Countries.");
-                        break;
-                    case ("-constit-top-5"):
-                        Console.WriteLine("Top Five Constituencies.");
-                        break;
-                    default:
-                        break;
+                    Console.WriteLine("Top Countries.");
+                }
+
+                if (arg.Contains("-constit-top-"))
+                {
+                    Console.WriteLine("Top Constituencies.");
                 }
             }
         }
 
         private static void RunCalculations(string[] args)
         {
-            //TODO: TotalVotes Outside UK
             GetTotals("outsideUK");
-            //TODO: TotalVotes Inside UK
+            Console.WriteLine("");
+
             GetTotals("insideUK");
+            Console.WriteLine("");
 
             foreach (var arg in args)
             {
-                switch (arg)
+                if (arg.Contains("-country-top-"))
                 {
-                    case ("-country-top-5"):
-                        GetTopFive("countries");
-                        break;
-                    case ("-constit-top-5"):
-                        GetTopFive("constituencies");
-                        break;
-                    default:
-                        break;
+                    var number = Convert.ToInt32(arg.Remove(0, 13));
+                    GetTop("countries",number);
                 }
+                else if (arg.Contains("-constit-top-"))
+                {
+                    var number = Convert.ToInt32(arg.Remove(0, 13));
+                    GetTop("constituencies", number);
+                }
+                    
+                Console.WriteLine("");
             }
         }
 
-        private static void GetTopFive(string ofWhat)
+        private static void GetTop(string ofWhat, int amount)
         {
             if (ofWhat == "countries")
             {
-                
+                var sorted = _petition.Countries.OrderByDescending(op => op.VoteCount).Take(amount).ToList();
+                var count = 0;
+                foreach (var item in sorted)
+                {
+                    count++;
+                    Console.WriteLine($"{count}. {item.Name} with {item.VoteCount} votes placed.");
+                }
             }
             else if (ofWhat == "constituencies")
             {
-                
+                var sorted = _petition.Constituencies.OrderByDescending(op => op.VoteCount).Take(amount).ToList();
+
+                var count = 0;
+                foreach (var item in sorted)
+                {
+                    count++;
+                    Console.WriteLine($"{count}. {item.Name}({item.MP}) with {item.VoteCount} votes placed.");
+                }
             }
         }
 
@@ -73,11 +144,27 @@ namespace petition_calculator
         {
             if (where == "outsideUK")
             {
-                
+                Console.Write("Getting total votes outside the UK: ");
+                var count = 0;
+                foreach (var country in _petition.Countries)
+                {
+
+                        count += country.VoteCount;
+
+                }
+
+                Console.Write($"{count}.\n");
             }
             else if (where == "insideUK")
             {
-                
+                Console.Write("Getting total votes inside the UK: ");
+                var count = 0;
+                foreach (var constituency in _petition.Constituencies)
+                {
+                    count += constituency.VoteCount;
+                }
+
+                Console.Write($"{count}.\n");
             }
         }
     }
